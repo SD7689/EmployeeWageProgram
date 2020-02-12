022a458c3f04d72600a15bd5834c0da4e5507d21
SD7689 <dewanganshivam89@gmail.com>
2020-02-12 18:52:34 +0530

    [Shivam]ADD.Merge UserCase5 Branch to Tmaster

--- empWageComputation.sh
+++ empWageComputation.sh
@@ -1,2 +1,9 @@
 #!/bin/sh
-echo "WELCOME TO EMPLOYEE WAGE COMPUTATION PROGRAM ON MASTER BRANCH"
+Monthly_Wage()
+{
+Working_Days = 20
+Daily_Wage = 160
+Partime_Wage = 120
+echo "Monthly Wage of Employe is: Rs$(($Working_Days*$Daily_Wage))"
+echo "Monthly Wage of a part Time Employee is: Rs$(($Partime_Wage*$Working_Days))"
+}

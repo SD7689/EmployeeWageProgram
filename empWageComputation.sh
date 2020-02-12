f06ca2b5ea40548443feddbea20146ee8c28c1bf
SD7689 <dewanganshivam89@gmail.com>
2020-02-12 16:40:43 +0530

    [Shivam]ADDED.a PartTime Wage function in program file

--- empWageComputation.sh
+++ empWageComputation.sh
@@ -1,2 +1,7 @@
-#!/bin/sh
-echo "WELCOME TO EMPLOYEE WAGE COMPUTATION PROGRAM ON MASTER BRANCH"
+PartTime_Wage()
+{
+PartTime_Hour=4
+Wages_Per_Hour=20
+echo "Part Time Wage Of a Employee is Rs$(($PartTime_Hour*Wages_Per_Hour))."
+}
+PartTime_Wage

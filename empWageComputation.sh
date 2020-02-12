178ff82d7db7543985463d4743e2cfd61181f671
SD7689 <dewanganshivam89@gmail.com>
2020-02-12 18:17:38 +0530

    [Shivam]ADDDED. a Swtich case statement in the program

--- empWageComputation.sh
+++ empWageComputation.sh
@@ -1,2 +1,31 @@
 #!/bin/sh
-echo "WELCOME TO EMPLOYEE WAGE COMPUTATION PROGRAM ON MASTER BRANCH"
+#UserCase_2
+DailyWage()
+{
+Wage_per_Hour=20
+FullDay_Hour=8
+echo "Employee Daily Wage is Rs$(($Wage_per_Hour*$FullDay_Hour)) of $FullDay_Hour hours"
+}
+#UserCase_3
+PartTime_Wage()
+{
+PartTime_Hour=4
+Wages_Per_Hour=20
+echo "Part Time Wage Of a Employee is Rs$(($PartTime_Hour*Wages_Per_Hour))."
+}
+echo "1.DailyWages \n 2.Partime Wage"
+echo "Enter one Option:"
+read option
+case $option in
+	1)
+		DailyWage
+		;;
+	2)
+		PartTimeWage
+		;;
+	*)
+		echo "Enter a Valid option"
+		;;
+esac
+
+

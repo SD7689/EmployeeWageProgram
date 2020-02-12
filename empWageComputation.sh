77ca201cd2a54a67577dcee8b9ac52e2f5666261
SD7689 <dewanganshivam89@gmail.com>
2020-02-12 16:28:11 +0530

    [Shivam]ADDED.a DailyWages function in program file

--- empWageComputation.sh
+++ empWageComputation.sh
@@ -1,2 +1,7 @@
-#!/bin/sh
-echo "WELCOME TO EMPLOYEE WAGE COMPUTATION PROGRAM ON MASTER BRANCH"
+echo "Daily Employee Wage:"
+DailyWage()
+{
+    Wage_per_Hour = 20
+FullDay_Hour = 8
+echo "Employee Wage is Rs$(($Wage_per_Hour*$FullDay_Hour)) of $FullDay_Hour hours"
+}

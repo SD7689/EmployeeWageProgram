a74350e353704fff91d057c306c70eccc51f14fe
SD7689 <dewanganshivam89@gmail.com>
2020-02-13 12:41:49 +0530

    [Shivam]ADD.added a Function of Conditional Monthly Wage Program in Branch

--- empWageComputation.sh
+++ empWageComputation.sh
@@ -1,2 +1,54 @@
 #!/bin/sh
-echo "WELCOME TO EMPLOYEE WAGE COMPUTATION PROGRAM ON MASTER BRANCH"
+echo "WELCOME TO EMPLOYEE WAGE COMPUTATION PROGRAM IN MASTER BRANCH"
+#use Case 1-
+CheckAttendance()
+{
+    	Attendance=$(($RANDOM%2))
+		if(($Attendance<1))
+		then
+            		return 0
+        	else
+            		return 1
+        	fi
+}
+CheckAttendance
+#UserCase_2
+DailyWage()
+{
+	Wage_per_Hour=20
+	FullDay_Hour=8
+	Dailywage=$(($FullDay_Hour*$Wage_per_Hour))
+	return $Dailywage
+	echo "Employee Daily Wage is Rs$Dailywage of $FullDay_Hour hours"
+}
+#UserCase_3
+PartTime_Wage()
+{
+	PartTime_Hour=4
+	Wages_Per_Hour=20
+	PartTime=$(($PartTime_Hour*$Wages_Per_Hour))
+	return $PartTime
+	echo "Part Time Wage Of a Employee is Rs$PartTime."
+}
+#UserCase_6
+CondMonthlyWage()
+{
+	for((i=0; i<20; i++))
+	do
+		CheckAttendance
+		res=$?
+		if(($res==0))
+		then
+			count=$((count+1))
+		fi
+	done
+	TotalWorkHour=$((8*count))
+	if(($TotalWorkHour<100 && count<=20))
+	then
+		MonthlyWages=$((TotalWorkHour*20))
+	else
+		MonthlyWages=$((100*20))
+	fi
+	echo "Monthly Payment of 100Hours/20Days is Rs"$MonthlyWages
+}
+CondMonthlyWage

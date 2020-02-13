295c8787c07697b510000d083fc47f2564903a43
SD7689 <dewanganshivam89@gmail.com>
2020-02-13 11:26:47 +0530

    [Shivam]ADD. some changes in Usercase1 Program

--- empWageComputation.sh
+++ empWageComputation.sh
@@ -1,14 +1,14 @@
 #use Case 1--
 CheckAttendance()
 {
-    Attendance =$(($RANDOM % 2))
-		if (($Attendance < 1))
+    Attendance=$(($RANDOM%2))
+		if(($Attendance<1))
 		then
-            echo "Employee is present:"
-
-        else
-        echo "Employee is Absent:"
-
-        fi
+            		echo "Employee is present:"
+			return 0
+	        else
+        		echo "Employee is Absent:"
+			return 1
+        	fi
 }
 CheckAttendance
